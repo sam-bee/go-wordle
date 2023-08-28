78b3e298d36cc93003bd6163c2be197d44302445
sam-bee <130986804+sam-bee@users.noreply.github.com>
2023-08-28 21:35:11 +0100

    To make it possible to use embed to access the data files, move code and data so that .csv files are in the relevant package dir

--- words/word_list_files.go
+++ words/word_list_files.go
@@ -1,43 +1,42 @@
-package infrastructure
+package words
 
 import (
 	"bufio"
 	"fmt"
 	"io"
 	"os"
-	"wordle/words"
 )
 
-const wordListValidGuessesFile = "./data/wordlist-valid-guesses.csv"
-const wordListValidSolutionsFile = "./data/wordlist-valid-solutions.csv"
+const wordListValidGuessesFile = "./words/data/wordlist-valid-guesses.csv"
+const wordListValidSolutionsFile = "./words/data/wordlist-valid-solutions.csv"
 
-func GetValidGuessesWordList(writer io.Writer) (words.WordList, error) {
+func GetValidGuessesWordList(writer io.Writer) (WordList, error) {
 	fmt.Fprintf(writer, "Reading from file: %s\n", wordListValidGuessesFile)
 	return makeWordListFromFile(wordListValidGuessesFile)
 }
 
-func GetValidSolutionsWordList(writer io.Writer) (words.WordList, error) {
+func GetValidSolutionsWordList(writer io.Writer) (WordList, error) {
 	fmt.Fprintf(writer, "Reading from file: %s\n", wordListValidSolutionsFile)
 	return makeWordListFromFile(wordListValidSolutionsFile)
 }
 
-func makeWordListFromFile(filename string) (words.WordList, error) {
+func makeWordListFromFile(filename string) (WordList, error) {
 
 	// To prepare to read the contents, open the file
 	file, err := os.Open(filename)
 	if err != nil {
-		return words.WordList{}, err
+		return WordList{}, err
 	}
 	defer file.Close()
 
 	// To provide a list of Words in the file, scan its contents
-	wordsFromFile := make([]words.Word, 0)
+	wordsFromFile := make([]Word, 0)
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
-		word := words.MakeWord(text)
+		word := MakeWord(text)
 		wordsFromFile = append(wordsFromFile, word)
 	}
-	return words.WordList{Words: wordsFromFile}, nil
+	return WordList{Words: wordsFromFile}, nil
 }
