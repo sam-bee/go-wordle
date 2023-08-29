f131b2aa3a8d16633a3b4035e6e5da44ee38113c
sam-bee <130986804+sam-bee@users.noreply.github.com>
2023-08-29 16:53:43 +0100

    To improve the test suite regarding the core Player algorithm, add more tests to player_test.go

--- player/player_test.go
+++ player/player_test.go
@@ -34,3 +34,35 @@ func TestGuessingAPossibleSolutionOnLastTurn(t *testing.T) {
 		t.Errorf("Because it's the last guess, guessing %q might have won the game, but Player guessed %q, which can't win", expected, got)
 	}
 }
+
+func TestItUnderstandsItIsReducingShortlist(t *testing.T) {
+
+	var validGuesses = []words.Word{"CHANT"}
+	var validSolutions = []words.Word{"SCARE", "SHARE", "SNARE", "SPARE", "STARE"}
+
+	p := NewPlayer(validSolutions, validGuesses)
+
+	_, evaluation := p.GetNextGuess(false)
+	got := evaluation.worstCaseShortlistCarryOverRatio
+	expected := 0.2
+
+	if got != expected {
+		t.Errorf("It reduces the shortlist by a factor of %.2f with a guess, but it seems to think the shortlist reduction is %.2f", expected, got)
+	}
+}
+
+func TestItUnderstandsBadGuessesDontReduceShortlist(t *testing.T) {
+
+	var validGuesses = []words.Word{"XXXXX"}
+	var validSolutions = []words.Word{"SCARE", "SHARE", "SNARE", "SPARE", "STARE"}
+
+	p := NewPlayer(validSolutions, validGuesses)
+
+	_, evaluation := p.GetNextGuess(false)
+	got := evaluation.worstCaseShortlistCarryOverRatio
+	expected := 1.0
+
+	if got != expected {
+		t.Errorf("If you force it to choose a really unhelpful guess, shortlist carry-over ratio is %.2f, but it thinks it is %.2f", expected, got)
+	}
+}
